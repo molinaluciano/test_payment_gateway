fde0bf75fcdfb90798ea86815f0cb06124fe707b
Luciano Molina <lmolina.s@cocha.com>
2021-05-30 21:53:53 -0400

    refactor testing

--- src/test/java/com/lmolina/service/PaymentServiceTest.java
+++ src/test/java/com/lmolina/service/PaymentServiceTest.java
@@ -1,29 +1,38 @@
 package com.lmolina.service;
 
 import com.lmolina.model.PaymentResponse;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class PaymentServiceTest {
+    private PaymentGatewayInterface paymentGatewayInt;
+    private  PaymentService paymentService;
+
+    @Before
+    public void setup(){
+         paymentGatewayInt = Mockito.mock(PaymentGatewayInterface.class);
+         paymentService = new PaymentService(paymentGatewayInt);
+    }
 
     @Test
     public  void payment_is_correct(){
-        PaymentGatewayInterface paymentGatewayInt = Mockito.mock(PaymentGatewayInterface.class);
+//        PaymentGatewayInterface paymentGatewayInt = Mockito.mock(PaymentGatewayInterface.class);
         Mockito.when(paymentGatewayInt.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
 
-        PaymentService paymentService = new PaymentService(paymentGatewayInt);
+//        PaymentService paymentService = new PaymentService(paymentGatewayInt);
         boolean result = paymentService.makePayment(1000);
         assertTrue(result);
     }
 
     @Test
     public  void payment_is_incorrect(){
-        PaymentGatewayInterface paymentGatewayInt = Mockito.mock(PaymentGatewayInterface.class);
+//        PaymentGatewayInterface paymentGatewayInt = Mockito.mock(PaymentGatewayInterface.class);
         Mockito.when(paymentGatewayInt.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
 
-        PaymentService paymentService = new PaymentService(paymentGatewayInt);
+//        PaymentService paymentService = new PaymentService(paymentGatewayInt);
         boolean result = paymentService.makePayment(1000);
         assertFalse(result);
     }
